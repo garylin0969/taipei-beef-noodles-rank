edd0017a600e56e8703a6335c606302b1133d06a
Gary <garylin0969@gmail.com>
2025-08-04 21:46:52 +0800

    feat: wrap App component with QueryProvider for React Query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App.tsx';
+import './index.css';
+import { QueryProvider } from './providers';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryProvider>
+            <App />
+        </QueryProvider>
+    </StrictMode>
+);
