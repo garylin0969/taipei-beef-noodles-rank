045b9b07fc9ce067432447880ad5bdb43d0d5047
Gary <garylin0969@gmail.com>
2025-08-04 23:06:10 +0800

    feat: add Footer component for site-wide footer display with copyright information

---
+++ src/components/organisms/footer/footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+        <footer className="border-t py-4 text-center text-sm tracking-wide">
+            <p>
+                &copy;{' '}
+                <span className="mx-1">
+                    <a
+                        className="hover:text-primary"
+                        href="https://github.com/garylin0969"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Gary Lin
+                    </a>
+                </span>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
