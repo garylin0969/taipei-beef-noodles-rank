57871074af56470739e5f68d4e0acc1117f89a93
Gary <garylin0969@gmail.com>
2025-08-04 23:06:15 +0800

    feat: add social media links to Header component for improved connectivity and user engagement

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,7 +1,46 @@
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const SOCIAL_LINKS = [
+    {
+        href: 'https://www.linkedin.com/in/garylin0969',
+        target: '_blank',
+        icon: FaLinkedin,
+        label: 'LinkedIn',
+    },
+    {
+        href: 'https://www.facebook.com/profile.php?id=100009915255579&mibextid=ZbWKwL',
+        target: '_blank',
+        icon: FaFacebook,
+        label: 'Facebook',
+    },
+    {
+        href: 'https://github.com/garylin0969/taipei-beef-noodles-rank',
+        target: '_blank',
+        icon: FaGithub,
+        label: 'GitHub',
+    },
+];
+
 const Header = () => {
     return (
         <header className="border-border/40 bg-background/60 sticky top-0 left-0 z-50 border-b shadow-md backdrop-blur-md">
-            <div className="container mx-auto flex h-14.5 items-center justify-between px-4"></div>
+            <div className="container mx-auto flex h-14.5 items-center justify-between px-4">
+                <div className="flex items-center gap-2">
+                    <img src="/favicons/favicon-32x32.png" alt="北市牛肉麵排行榜" className="h-8 w-8" />
+                    <h1 className="text-2xl font-bold">北市牛肉麵排行榜</h1>
+                </div>
+            </div>
+            <div className="bg-primary/5 text-primary">
+                <div className="container mx-auto">
+                    <div className="flex h-8 items-center justify-center gap-x-2 py-1">
+                        {SOCIAL_LINKS.map((link) => (
+                            <a key={link.label} href={link.href} target={link.target} rel="noopener noreferrer">
+                                <link.icon className="size-6" />
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
