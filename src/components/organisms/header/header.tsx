c52ffa9573d7018b73c8b4d5a8a8fcd964d71d11
Gary <garylin0969@gmail.com>
2025-08-04 22:50:16 +0800

    feat: update index.html with new title and meta tags for SEO, replace favicon with new images, and add header component for improved layout

---
+++ src/components/organisms/header/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="border-border/40 bg-background/60 sticky top-0 left-0 z-50 border-b shadow-md backdrop-blur-md">
+            <div className="container mx-auto flex h-14.5 items-center justify-between px-4"></div>
+        </header>
+    );
+};
+
+export default Header;
